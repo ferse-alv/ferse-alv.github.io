1742cd9490ca0c57e995cdf57d8086378058a792
Fernando <ferngruisu1@gmail.com>
2023-11-30 23:37:10 -0600

    updated app name to 'fserrano-dev'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-angular-project' title`, () => {
+  it(`should have the 'fserrano-dev' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-angular-project');
+    expect(app.title).toEqual('fserrano-dev');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-angular-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fserrano-dev');
   });
 });
